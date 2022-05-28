58cbb8d6acf4b74d778829a1e4483944db5e330b
Mohammed Patel <patmohd@hotmail.com>
2022-05-28 16:26:25 +0400

    more func

--- vec_test.cpp
+++ vec_test.cpp
@@ -6,34 +6,53 @@
 
 int main(void)
 {
-    ft::vector <int> v(10, 5);
-	std::vector<int> s(10, 5);
+    ft::vector <int> mine(10, 5);
+	std::vector<int> orig(10, 5);
 
-    // std::vector<int>::iterator::o
-    ft::vector<int>::iterator it = v.begin();
-    // it++;
-    // std::vector<int>::iterator::
-    // ptr = s.begin();
-    // ptr--;
+    ft::vector<int>::iterator mIter = mine.begin();
+    std::vector<int>::iterator oIter = orig.begin();
 
-    std::cout << "mine --- begin: " << v.begin() << std::endl;
-    std::cout << "orig --- begin: " << s.begin() << std::endl;
+    std::cout << "mine --- capacity: " << mine.capacity() << std::endl;
+    std::cout << "orig --- capacity: " << orig.capacity() << std::endl;
 
-    // size_t ptr = s.s();
-    // std::cout << "mine --- size: " << v.size() << std::endl;
-    // std::cout << "orig --- size: " << v.size() << std::endl;
+    std::cout << "mine --- not empty: " << mine.empty() << std::endl;
+    std::cout << "orig --- not empty: " << orig.empty() << std::endl;
 
-    // std::cout << "mine --- max size: " << v.max_size() << std::endl;
-    // std::cout << "orig --- max size: " << s.max_size() << std::endl;
+    ft::vector <int> test_mine;
+	std::vector<int> test_orig;
 
-    // v.resize(5, 2);
-    // s.resize(6, 2);
+    std::cout << "mine --- empty: " << test_mine.empty() << std::endl;
+    std::cout << "orig --- empty: " << test_orig.empty() << std::endl;
 
-    // std::cout << "mine --- resize: " << v.size() << std::endl;
-    // std::cout << "orig --- resize: " << s.size() << std::endl;
+    std::cout << "mine --- begin: " << *mIter << std::endl;
+    std::cout << "orig --- begin: " << *oIter << std::endl;
 
-    // std::cout << "orig --- capacity: " << s.capacity() << std::endl;
-    // std::cout << "mine --- capacity: " << s.capacity() << std::endl;
+    // ft::vector<int>::const_iterator ct_mIter = mine.begin();
+    // std::vector<int>::const_iterator ct_oIter = orig.begin();
+
+    // std::cout << "mine --- begin: " << *mIter << std::endl;
+    // std::cout << "orig --- begin: " << *ct_oIter << std::endl;
+
+    mIter = mine.end();
+    oIter = orig.end();
+
+    std::cout << "mine --- end: " << *mIter << std::endl;
+    std::cout << "orig --- end: " << *oIter << std::endl;
+
+    size_t oPtr = orig.size();
+    size_t mPtr = mine.size();
+
+    std::cout << "mine --- size: " << mine.size() << std::endl;
+    std::cout << "orig --- size: " << orig.size() << std::endl;
+
+    std::cout << "mine --- max size: " << mine.max_size() << std::endl;
+    std::cout << "orig --- max size: " << orig.max_size() << std::endl;
+
+    // mine.resize(5, 2);
+    // orig.resize(6, 2);
+
+    // std::cout << "mine --- resize: " << mine.size() << std::endl;
+    // std::cout << "orig --- resize: " << orig.size() << std::endl;
 
     return (0);
 }
--- vector.hpp
+++ vector.hpp
@@ -31,15 +31,15 @@ namespace ft
         typedef typename allocator_type::pointer            pointer;
         typedef typename allocator_type::const_pointer        const_pointer;
         typedef typename allocator_type::size_type            size_type;
-        // typedef pointer                                        iterator;
         typedef ft::vectorIterator<value_type>                iterator;
+        typedef ft::vectorIterator<const value_type>          const_iterator;
         // typedef ft::vectorRevIterator<value_type>            reverse_iterator;
         // have to add iterators
     private:
-        allocator_type    _alloc;
-        pointer            _begin;
-        pointer            _end;
-        size_type        _size;
+        allocator_type      _alloc;
+        pointer             _begin;
+        pointer             _end;
+        size_type           _size;
 
     public:
         explicit vector(const allocator_type& alloc = allocator_type()) : _alloc(alloc), _begin(NULL), _end(NULL), _size(0) {};
@@ -51,8 +51,8 @@ namespace ft
                         _size(0)
                         {
                             _begin = _alloc.allocate(n * 2);
-                            _end = _begin;
                             _size = n;
+                            _end = _begin + _size;
                             for (int i = 0; i < _size; i++)
                                 _alloc.construct(&_begin[i], val);
                         };
@@ -63,13 +63,15 @@ namespace ft
         // Iterators
 
         // begin();
-        iterator    begin(void)
-        {
+        iterator    begin() {
             return (iterator(this->_begin));
         }
+        // const begin()
+        const_iterator  begin() const {
+            return (const_iterator(this->begin));
+        }
         // end();
-        iterator    end()
-        {
+        iterator    end() {
             return (iterator(this->_end));
         }
         // rbegin(); have to do cons
@@ -90,9 +92,9 @@ namespace ft
             return (_size);
         }
         // max_size();
-        // size_type    max_size() const {
-        //     return (allocator_type().max_size());
-        // }
+        size_type    max_size() const {
+            return (allocator_type().max_size());
+        }
         // resize();
         // void    resize(size_type n, value_type val = value_type()) {
         //     size_type    _c_size = size();
@@ -101,7 +103,7 @@ namespace ft
         //         iterator    _iter;
         //         size_type    _capa = capacity();
         //         size_type    _diff = n - _c_size;
-        //         if (_sum <= _capa && _capa <= _capa - _diff)
+        //         if (_diff <= _capa && _capa <= _capa - _diff)
         //         {
         //             _iter = end();
         //             _size += _diff;
@@ -114,11 +116,21 @@ namespace ft
         //     }
         // }
         // capacity();
-        // size_type    capacity() const {
-        //     return static_cast<size_type>();
-        // }
+        size_type    capacity() const {
+            return static_cast<size_type>(_size);
+        }
         // empty();
+        bool    empty() const {
+            return (_begin == _end);
+        }
         // reserve();
+
+        void    reserve(size_type n) {
+            size_t __cap = _size;
+            if (__cap < n) {
+
+            }
+        }
         // shrink_to_fit();
 
         // Element access
@@ -144,6 +156,20 @@ namespace ft
         // Allocator
 
         // get_allocator();
+
+        void    replace(size_type len) {
+            pointer tmp = _begin;
+
+            for (iterator i = begin(); i <= end(); i++)
+
+        }
+
+        void    delete(pointer _tbd) {
+            size_type  _len = _size * 2;
+
+            for (size_type i = 0; i < _len; i++)
+                _alloc.destroy(&_tbd[i]);
+        }
     };
 }
 
